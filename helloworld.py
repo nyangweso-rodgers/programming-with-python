28f6880507d1acebfd63b4676cb693d9832683c6
Rodgers Nyangweso <rodgerso65@gmail.com>
2022-06-16 22:17:22 +0300

    added a helloworld file

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello World!")
+    name = input("What is your name?")
+    print("Nice to meet you", name)
+    
+# the below lines of code implies the file is executed as an independent Python program
+if __name__ == " __main__ ":
+    main()
