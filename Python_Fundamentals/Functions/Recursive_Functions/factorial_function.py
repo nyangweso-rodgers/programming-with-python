d17d4cb6f8c7bf5d62764a25bc07fcc6c42e466a
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-02-15 16:27:20 +0300

    deleted recursive functions folder

--- Python_Fundamentals/Functions/Recursive_Functions/factorial_function.py
+++
@@ -1,7 +0,0 @@
-# factorial function
-
-## Example 1: Factorial function using a recursive function
-## Recusrsive definition of Factorial function
-def factorial(n):
-     return 1 if n <= 1 else n * factorial(n - 1)
-print(factorial(5))
