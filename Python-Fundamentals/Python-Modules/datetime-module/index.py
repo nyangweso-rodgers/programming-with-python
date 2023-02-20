366a751d73ea917e9f0c18b2a2abba36d5586e3b
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-02-20 21:43:16 +0300

    ++ Python Modules with initial commit on datetime module

---
+++ Python-Fundamentals/Python-Modules/datetime-module/index.py
@@ -0,0 +1,13 @@
+import datetime as dt
+
+# Get Current Date and Time using now() method
+now = dt.datetime.now()
+print(now)
+
+# Current Date
+current_date = dt.date.today()
+print(current_date)
+
+# Python datetime.date Class
+d = dt.date(2022, 2, 20)
+print(d)
