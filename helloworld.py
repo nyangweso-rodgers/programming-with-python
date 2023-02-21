11a67dfde43eef9dd1d39932d8074cb22a771ee8
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-02-21 23:11:26 +0300

    deleted a file

--- helloworld.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print("Hello World!")
-    name = input("What is your name?")
-    print("Nice to meet you", name)
-    
-# the below lines of code implies the file is executed as an independent Python program
-if __name__ == " __main__ ":
-    main()
