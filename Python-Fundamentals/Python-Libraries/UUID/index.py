a0adbbce1675ebb7d71ec65f1b520dfd843ff265
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-02-21 22:14:59 +0300

    ++ Python Library folder with initial commit on uuid and pytube libraries

---
+++ Python-Fundamentals/Python-Libraries/UUID/index.py
@@ -0,0 +1,6 @@
+import uuid
+print(dir(uuid))
+
+print ("The random id using uuid1() is :", uuid.uuid1()) 
+
+print(str(uuid.uuid4()))
---
+++ Python-Fundamentals/Python-Libraries/pytube/how-to-download-youtube-video.py
@@ -0,0 +1,10 @@
+from pytube import YouTube
+
+# check the methods
+# print(dir(YouTube))
+
+DOWNLOAD_FOLDER = "/Users/HP/Downloads"
+video_url = "https://www.youtube.com/watch?v=Ymkl0t0FOcw&list=RDYmkl0t0FOcw&start_radio=1"
+video_obj = YouTube(video_url)
+stream = video_obj.streams.get_highest_resolution()
+stream.download(DOWNLOAD_FOLDER)
