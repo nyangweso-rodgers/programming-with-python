a6c831d68604c0e5f991e95be6700e58b6d3b15e
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-02-21 22:22:12 +0300

    ++ sys python module

---
+++ Python-Fundamentals/Python-Modules/sys-module/index.py
@@ -0,0 +1,15 @@
+import sys
+
+# check all the methods
+# print(dir(sys))
+
+# Getting the Pyhon executable path
+print(sys.executable)
+
+# Getting Python Platform
+print(sys.platform)
+
+# Python version
+print(sys.version)
+
+print(sys.path)
