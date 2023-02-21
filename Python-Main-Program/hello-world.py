ff62bd74ec9a464e936b3b11a53c841434d861bb
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-02-21 23:16:09 +0300

    ++ python's main function

---
+++ Python-Main-Program/hello-world.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello World!")
+    
+# the below lines of code implies the file is executed as an independent Python program
+if __name__ == " __main__ ":
+    main()
---
+++ Python-Main-Program/index.py
@@ -0,0 +1,8 @@
+
+"""
+    So, the code if __name__ == "__main__": checks whether the script is being run as the main program (i.e., not imported as a module). 
+    If this condition is true, the main() function is called, which typically contains the main logic of the script.
+"""
+# the below lines of code implies the file is executed as an independent Python program
+if __name__ == " __main__ ":
+    main()
