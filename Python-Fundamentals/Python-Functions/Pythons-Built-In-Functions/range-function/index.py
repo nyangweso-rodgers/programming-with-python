89150e3d6b3fc3a361ff1fe855cee46d9aa47a31
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-02-26 16:18:47 +0300

    ++ lists of python built-in functions with initial commit on the range function

---
+++ Python-Fundamentals/Python-Functions/Pythons-Built-In-Functions/range-function/index.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    print(i)
+    
+print("#################")   
+for j in range(5, 9):
+    print(j)
+    
+print("###############")
+# Print even numbers in a range
+for z in range(0, 10, 2):
+    print(z)
