bcb5947a12abb28957edebee173d5cfaea1761b6
nyangweso-rodgers <rodgerso65@gmail.com>
2025-08-10 22:41:36 +0300

    Delete folder

--- 02-fundamentals/object-oriented-programming/Getting-Started-with-OOP-in-Python/how-to-create-a-class-using-constructor.py
+++
@@ -1,8 +0,0 @@
-class Soldier:
-    def __init__(self, armor, num_of_weapons):
-        self.armor = armor
-        self.num_of_weapons = num_of_weapons
-        
-soldier_one = Soldier(5, 10)
-print(soldier_one.armor)
-print(soldier_one.num_of_weapons)
