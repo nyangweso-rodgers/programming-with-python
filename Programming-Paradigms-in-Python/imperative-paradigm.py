7f251f4412c894f4d84cb265aa1f302d4f2314fb
nyangweso-rodgers <rodgerso65@gmail.com>
2025-10-05 15:08:25 +0300

    delete Programming-Paradigms-in-Python

--- Programming-Paradigms-in-Python/imperative-paradigm.py
+++
@@ -1,9 +0,0 @@
-# sum the lists results
-my_lists = [1, 2, 3, 4, 5]
-
-sum_of_list = 0
-
-for x in my_lists:
-    sum_of_list += x
-    
-print(sum_of_list)
--- Programming-Paradigms-in-Python/procedural-paradigm.py
+++
@@ -1,8 +0,0 @@
-def stringify(characters):
-    string = ''
-    for c in characters:
-        string = string + c
-    return stringify
-
-sample_characters = ['p', 'y', 't', 'h', 'o', 'n']
-print(stringify(sample_characters))
